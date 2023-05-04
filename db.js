7629a7c84648afe07afe4cc36b5a5d9af530cc27
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-05-04 23:34:37 +0530

    enviroment variables added

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook"; 
+const mongoURI = process.env.mongoURI; 
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI, ()=>{
